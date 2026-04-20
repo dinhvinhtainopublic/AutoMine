b300048779a63ad64ca22c744776dbdbb27c1d3e
dinhvinhtainopublic <dinhvinhtai.2k9@gmail.com>
2026-04-20 16:06:06 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
+source config.sh
+
 echo "[INFO] Starting miner..."
-
-THREADS=$(nproc)
-WORKER="devbox-rig"
-
-echo "[INFO] CPU threads: $THREADS"
+echo "[INFO] Wallet: $WALLET"
+echo "[INFO] Pool: $POOL"
 echo "[INFO] Worker: $WORKER"
 
-# Download xmrig nếu chưa có
+THREADS=$(nproc)
+echo "[INFO] CPU threads: $THREADS"
+
 if [ ! -f "xmrig" ]; then
     echo "[INFO] Downloading xmrig..."
-
     URL=$(wget -qO- https://api.github.com/repos/xmrig/xmrig/releases/latest \
     | grep browser_download_url \
     | grep linux-static-x64.tar.gz \
     | cut -d '"' -f 4)
-
     wget "$URL" -O xmrig.tar.gz
-
     tar -xvf xmrig.tar.gz
     mv xmrig-*/xmrig .
     chmod +x xmrig
+    rm -f xmrig.tar.gz
 fi
 
-# Run miner (NO hugepage, NO msr, NO 1GB page)
 ./xmrig \
--o pool.supportxmr.com:3333 \
--u WALLET_CUA_BAN \
--k \
--t $THREADS \
---cpu-affinity=$((THREADS-1)) \
---huge-pages=0 \
---randomx-1gb-pages=0 \
---donate-level=1
+  -o $POOL \
+  -u $WALLET \
+  -p $WORKER \
+  -k \
+  -t $THREADS \
+  --huge-pages=0 \
+  --randomx-1gb-pages=0 \
+  --donate-level=1
