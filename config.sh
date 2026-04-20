17ebf54040aea049a651bac3762aa723d1a054a2
dinhvinhtainopublic <dinhvinhtai.2k9@gmail.com>
2026-04-20 14:20:30 +0700

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# ===== CONFIG =====
+WALLET="4ByeEKTJbi3faVNHTWEupmM1fdwEv95CqCqC7rCDdVhXDt4vj5E4FB1jUKxNAF6EHFHmuQhnHoXcUK84Nc4cQfmfKQ8zXo5FtUE2QGuM6K"
+POOL="pool.supportxmr.com:3333"
+WORKER="devbox-rig"
+CPU_LIMIT="75"
+# ==================
