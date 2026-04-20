f2e3d644151ec4549b5380ee8a4e5e2d4f24fa8d
dinhvinhtainopublic <dinhvinhtai.2k9@gmail.com>
2026-04-20 16:57:49 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-
 # ===== CONFIG =====
 WALLET="4ByeEKTJbi3faVNHTWEupmM1fdwEv95CqCqC7rCDdVhXDt4vj5E4FB1jUKxNAF6EHFHmuQhnHoXcUK84Nc4cQfmfKQ8zXo5FtUE2QGuM6K"
 POOL="gulf.moneroocean.stream:10128"
-WORKER="my-rig"
+WORKER="rig-$(hostname)-$(cat /proc/sys/kernel/random/boot_id | cut -c1-8)"
 # ==================
