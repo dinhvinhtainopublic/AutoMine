152aed2eb764990558d5981eb56985afb439e964
dinhvinhtainopublic <dinhvinhtai.2k9@gmail.com>
2026-04-20 14:38:05 +0700

    Update config.sh

--- config.sh
+++ config.sh
@@ -4,5 +4,5 @@
 WALLET="4ByeEKTJbi3faVNHTWEupmM1fdwEv95CqCqC7rCDdVhXDt4vj5E4FB1jUKxNAF6EHFHmuQhnHoXcUK84Nc4cQfmfKQ8zXo5FtUE2QGuM6K"
 POOL="pool.supportxmr.com:3333"
 WORKER="devbox-rig"
-CPU_LIMIT="75"
+CPU_LIMIT="100"
 # ==================
